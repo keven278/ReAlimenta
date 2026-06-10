91085cb1603b99d57c71e3e8d7d36b1fa493c851
SEU NOME <SEU EMAIL DO GITHUB>
2026-06-10 12:02:07 -0300

    Fazendo ComercianteDAO

--- src/dao/ComercianteDAO.java
+++ src/dao/ComercianteDAO.java
@@ -1,4 +1,22 @@
 package dao;
+import model.Comerciante;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
 
 public class ComercianteDAO {
+    String sql = "INSERT INTO comerciante(nome, telefone, senha, cnpj, endereco, nome_loja) VALUES (?, ?, ?, ?, ?, ?)";
+
+    try(
+            Connection conn = Conexao.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql)
+
+    ) {
+        statement.setString(1, comerciante.getnome());
+
+
+    }
 }
