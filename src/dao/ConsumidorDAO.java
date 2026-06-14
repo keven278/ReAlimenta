04d6b95a30748f4f5f021b9e4d820d6a71ab0a17
SEU NOME <SEU EMAIL DO GITHUB>
2026-06-14 17:54:03 -0300

    Fazendo a classe DAO

--- src/dao/ConsumidorDAO.java
+++ src/dao/ConsumidorDAO.java
@@ -1,5 +1,37 @@
 package dao;
 
-public class ConsumidorDAO {
+import model.Comerciante;
+import model.Consumidor;
+import util.Conexao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ConsumidorDAO {
+    public void inserir(Consumidor consumidor) {
+        String sql ="INSERT INTO consumidor" + "nome, telefone, senha, cpf, emaiL" +
+                "VALUES (?, ?, ?, ?, ?, ?)";
+
+
+        try(
+                Connection conn = Conexao.getConnection();
+                PreparedStatement statement = conn.prepareStatement(sql)
+        ){
+            statement.setString(1, consumidor.getNome());
+            statement.setString(2, consumidor.getTelefone());
+            statement.setString(3, consumidor.getSenha());
+            statement.setString(4, consumidor.getCpf());
+            statement.setString(5, consumidor.getEmail());
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println("Erro ao inserir comerciante: " + e.getMessage());
+        }
+    }
 }
+
+
+
+
